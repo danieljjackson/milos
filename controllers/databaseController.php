512be9aa1af1c588e2fe47fb04fce51a40e16c6e
justtoconfirm <office@justtoconfirm.co.uk>
2023-11-03 17:10:42 +0000

    feat: add database controller to allow functions to interact with the database within the application

---
+++ controllers/databaseController.php
@@ -0,0 +1,17 @@
+<?php
+	require "models/databaseModel.php";
+
+	class DatabaseController {
+		private $dbModel;
+		
+		// Constructor
+		public function __construct() {
+			// Create a new instance of the Database Model to connect to the database and pass the database credentials
+			$this->dbModel = new DatabaseModel("localhost", "username", "password", "miloDB");
+		}
+
+		// Test function for testing we can call functions from the controller
+		public function test() {
+			echo "This is a test function";
+		}
+	}
